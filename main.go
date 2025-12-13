bdfc43fe3318d09f0de657e4219487224c566aee
Shoga Ayodeji <ayodejishoga1@gmail.com>
2025-12-13 20:33:41 +0100

    Interfaces

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var rect = Rectangle{Width: 10, Height: 20}
+	var area float64 = calculateArea(rect)
+	fmt.Println("Area is", area)
+
+}
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width, Height float64
+}
+
+func calculateArea(s Shape) float64 {
+	return s.Area()
+}
+
+func (r Rectangle) Area() float64 {
+	var area float64 = r.Width * r.Height
+	return area
 }
