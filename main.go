413148ae3809f17c1fb9367d7d565b7a1ea5f669
Shoga Ayodeji <ayodejishoga1@gmail.com>
2025-12-14 23:11:52 +0100

    Interface

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+)
 
+func main() {
+	var newCircle Circle = Circle{
+		radius: 6,
+	}
+	var newRectangle Rectangle = Rectangle{
+		height: 12,
+		width:  15,
+	}
+
+	var CircleArea float64 = newCircle.Area()
+	var RectangleArea float64 = newRectangle.Area()
+
+	fmt.Println("Area of circle is: ", CircleArea)
+	fmt.Println("Area of rectangle is: ", RectangleArea)
+	var buff io.Reader
+	fmt.Fscan(buff)
 }
---
+++ shapes.go
@@ -0,0 +1,26 @@
+package main
+
+import "math"
+
+type Circle struct {
+	radius float64
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+type Shape interface {
+	Area()
+}
+
+func (c Circle) Area() float64 {
+	var area float64 = math.Pi * (c.radius * c.radius)
+	return area
+}
+
+func (r Rectangle) Area() float64 {
+	var area float64 = r.height * r.width
+	return area
+}
